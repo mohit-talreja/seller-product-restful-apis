39783caeee7974661e87f411114c06eb070b8689
mohit-talreja <74865221+mohit-talreja@users.noreply.github.com>
2021-03-12 10:43:29 +0530

    Delete seller-product-restful-apis directory

--- seller-product-restful-apis/models/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-const dbString = 'mongodb://localhost:27017/SellerProductAPIs'
-
-mongoose
-    .connect(dbString, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(res => console.log('Connected to MongoDB.'))
-    .catch(err => console.log('Error in connecting to MongoDB!'))
